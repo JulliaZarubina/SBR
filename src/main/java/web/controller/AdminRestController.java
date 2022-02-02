f9055995c420a8c3dd3ea3ca7c35a871da61d261
JulliaZarubina <zarulia@bk.ru>
2022-02-02 22:30:10 +0300

    Commit

--- src/main/java/web/controller/AdminRestController.java
+++ src/main/java/web/controller/AdminRestController.java
@@ -1,6 +1,8 @@
 package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import web.model.User;
 import web.service.UserService;
@@ -18,27 +20,29 @@ public class AdminRestController {
         this.userService = userService;
     }
 
+
     @GetMapping("/users")
-    public List<User> getUsers() {
-        return userService.allUsers();
+    public ResponseEntity<List<User>> allUsers() {
+        return new ResponseEntity<>(userService.allUsers(), HttpStatus.OK);
     }
+
 
     @PostMapping("/users")
-    public User newUser(@RequestBody User user) {
+    public ResponseEntity<User> newUser(@RequestBody User user) {
         userService.add(user);
-        return user;
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
-    @PatchMapping("/users/{id}")
-    public User updateUser(@RequestBody User user, @PathVariable("id") int id) {
-        user.setId(id);
-        userService.edit(user);
-        return user;
+
+    @PutMapping()
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+        userService.add(user);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
-    @DeleteMapping("/users/{id}")
+
+    @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable("id") int id) {
         userService.delete(id);
     }
-
 }
